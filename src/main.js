24176cbc59ce6e4f99c2cdbdde956ba3d187df32
Oleg <Vanzhula.oleg@gmail.com>
2020-09-03 21:59:03 +0300

    Validation form login (Vuelidate)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "materialize-css/dist/js/materialize.min";
 import dateFilter from "./filters/date.filter";
+import "./registerServiceWorker";
+import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 //1param name filter, 2 function
 Vue.filter("date", dateFilter);
 
