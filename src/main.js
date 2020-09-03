3c9997abcbbc5340eef60aef3726e0006672c95a
Oleg <Vanzhula.oleg@gmail.com>
2020-09-03 17:47:43 +0300

    added functionality datetime, dropdown in navbar and btn add

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
-import 'materialize-css/dist/js/materialize.min';
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import "materialize-css/dist/js/materialize.min";
+import dateFilter from "./filters/date.filter";
 
 Vue.config.productionTip = false;
+//1param name filter, 2 function
+Vue.filter("date", dateFilter);
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
