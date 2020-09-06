9ed1dbec669794b6d6253eb2e91f22bc1736d928
Oleg <Vanzhula.oleg@gmail.com>
2020-09-06 22:14:59 +0300

    logging records(history) and protecting routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
+import firebase from "firebase/app";
 
 Vue.use(VueRouter);
 
@@ -15,49 +16,49 @@ const routes = [
     path: "/register",
     name: "register",
     meta: { layout: "empty" },
-     //lezy loading
+    //lezy loading
     component: () => import("../views/Register.vue")
   },
   {
     path: "/",
     name: "home",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/Home.vue")
   },
   {
     path: "/categories",
     name: "categories",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/Categories.vue")
   },
   {
     path: "/detail/:id",
     name: "detail",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/Detail.vue")
   },
   {
     path: "/history",
     name: "history",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/History.vue")
   },
   {
     path: "/planning",
     name: "planning",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/Planning.vue")
   },
   {
     path: "/profile",
     name: "profile",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/Profile.vue")
   },
   {
     path: "/record",
     name: "record",
-    meta: { layout: "main" },
+    meta: { layout: "main", auth: true },
     component: () => import("../views/Record.vue")
   }
 ];
@@ -66,6 +67,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requireAuth = to.matched.some(record => record.meta.auth);
+  if (requireAuth && !currentUser) {
+    next("/login?message=login");
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
   logout: "Вы вышли из системы",
+  login: "Для начала войдите в сестему",
   "auth/user-not-found": "Пользователь с таким email не существует",
   "auth/wrong-password": "Неверный пароль",
   "auth/email-already-in-use": "email уже занят"
